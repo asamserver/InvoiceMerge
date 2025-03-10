f889471ebfe2556af29489951b240ef54b9e9a33
Milad Yousefi <yousefimiladdd@gmail.com>
2025-03-10 16:17:38 +0330

    remove invoice merge hook

--- lib/Service/Hook.php
+++ lib/Service/Hook.php
@@ -181,31 +181,43 @@ class Hook
                                     <strong>Total:</strong> $${invoice.total} USD
                                 `;
 
-                                
-                                    let buttonContainer = document.createElement("div");
-                                    let payButton = document.createElement("a");
-                                    payButton.href = `viewinvoice.php?id=${invoiceId}`;
-                                    payButton.textContent = "Pay";
-                                    payButton.style.border = "2px solid #ccc";
-                                    payButton.style.color = "green";
-                                    payButton.style.padding = "5px 30px";
-                                    payButton.style.textDecoration = "none";
-                                    payButton.style.marginRight = "10px";
-                                    payButton.style.borderRadius = "3px";
+                              if (invoice.status === "Payment Pending") {
+                                let buttonContainer = document.createElement("div");
+                                let payButton = document.createElement("div");
+                                payButton.textContent = "Payment Pending";
+                                payButton.style.border = "2px solid #ccc";
+                                payButton.style.color = "blue";
+                                payButton.style.padding = "5px 30px";
+                                payButton.style.textDecoration = "none";
+                                payButton.style.marginRight = "10px";
+                                payButton.style.borderRadius = "3px";
 
+                                buttonContainer.appendChild(payButton);
+                            } else {
+                                let buttonContainer = document.createElement("div");
+                                let payButton = document.createElement("a");
+                                payButton.href = `viewinvoice.php?id=${invoiceId}`;
+                                payButton.textContent = "Pay";
+                                payButton.style.border = "2px solid #ccc";
+                                payButton.style.color = "green";
+                                payButton.style.padding = "5px 30px";
+                                payButton.style.textDecoration = "none";
+                                payButton.style.marginRight = "10px";
+                                payButton.style.borderRadius = "3px";
 
-                                        let cancelButton = document.createElement("a");
-                                        cancelButton.href = `index.php?m=InvoiceMerge&action=cancell_invoice&invoice_id=${invoiceId}`;
-                                        cancelButton.textContent = "Cancel";
-                                        cancelButton.style.border = "2px solid #ccc";
-                                        cancelButton.style.color = "red";
-                                        cancelButton.style.padding = "5px 30px";
-                                        cancelButton.style.borderRadius = "3px";
-                                        cancelButton.style.textDecoration = "none";
+                                let cancelButton = document.createElement("a");
+                                cancelButton.href = `index.php?m=InvoiceMerge&action=cancell_invoice&invoice_id=${invoiceId}`;
+                                cancelButton.textContent = "Cancel";
+                                cancelButton.style.border = "2px solid #ccc";
+                                cancelButton.style.color = "red";
+                                cancelButton.style.padding = "5px 30px";
+                                cancelButton.style.borderRadius = "3px";
+                                cancelButton.style.textDecoration = "none";
 
+                                buttonContainer.appendChild(payButton);
+                                buttonContainer.appendChild(cancelButton);
+                            }
 
-                                        buttonContainer.appendChild(payButton);
-                                        buttonContainer.appendChild(cancelButton);
 
 
 
