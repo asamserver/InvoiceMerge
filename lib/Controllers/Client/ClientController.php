20a06c3a1760dd2bdf64f034330146854611cd3e
Milad Yousefi <yousefimiladdd@gmail.com>
2025-03-11 16:57:02 +0330

    Cart Item deletion error

--- lib/Controllers/Client/ClientController.php
+++ lib/Controllers/Client/ClientController.php
@@ -73,7 +73,7 @@ class ClientController
                         ->where('type', 'Invoice')
                         ->where('userid', $userId);
                 })
-                ->where('status', 'Unpaid')
+                ->whereIn('status', ['Unpaid','Payment Pending'])
                 ->exists();
     
             if ($unpaidInvoiceExists) {
